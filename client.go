313fee0570a3fbbe3635c2f8418b3cdd3e661943
Alex Guerrieri <klaidliadon@gmail.com>
2020-09-17 14:27:09 +0200

    add context, fix refresh

--- client.go
+++ client.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -14,7 +15,7 @@ import (
 	"golang.org/x/oauth2/google"
 )
 
-func NewClient(credentials, token string) (*http.Client, error) {
+func NewClient(ctx context.Context, credentials, token string) (*http.Client, error) {
 	b, err := ioutil.ReadFile(credentials)
 	if err != nil {
 		return nil, err
@@ -33,33 +34,37 @@ func NewClient(credentials, token string) (*http.Client, error) {
 		defer f.Close()
 		t = new(oauth2.Token)
 		if err = json.NewDecoder(f).Decode(t); err != nil {
-			fmt.Println("a")
 			return nil, err
+		}
+		if t.Expiry.After(time.Now()) {
+			return config.Client(ctx, t), nil
 		}
 	}
-	if t == nil || t.Expiry.Before(time.Now()) {
-		authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-		browser.OpenURL(authURL)
-		fmt.Printf("Access required: to enable access for your account use the sign-in page opened in your browser or the following link:\n%s\n", authURL)
-		ch := make(chan string)
-		go func() {
-			http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-				ch <- r.URL.Query().Get("code")
-				fmt.Fprint(w, "You can close this tab.")
-			})
-			http.ListenAndServe("localhost:8192", nil)
-		}()
-		if t, err = config.Exchange(context.TODO(), <-ch); err != nil {
-			return nil, err
+
+	url := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	_ = browser.OpenURL(url)
+	fmt.Printf("Access required: to enable access for your account use the sign-in page opened in your browser or the following link:\n%s\n", url)
+	ch := make(chan string)
+	go func(ch chan<- string) {
+		defer close(ch)
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			ch <- r.URL.Query().Get("code")
+			fmt.Fprint(w, "You can close this tab.")
+		})
+		if err := http.ListenAndServe("localhost:8192", nil); err != nil {
+			log.Fatalln("Cannot start server:", err)
 		}
-		f, err := os.OpenFile(token, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-		if err != nil {
-			return nil, err
-		}
-		defer f.Close()
-		if err := json.NewEncoder(f).Encode(t); err != nil {
-			return nil, err
-		}
+	}(ch)
+	if t, err = config.Exchange(ctx, <-ch); err != nil {
+		return nil, err
 	}
-	return config.Client(context.Background(), t), nil
+	f, err := os.OpenFile(token, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	if err := json.NewEncoder(f).Encode(t); err != nil {
+		return nil, err
+	}
+	return config.Client(ctx, t), nil
 }
